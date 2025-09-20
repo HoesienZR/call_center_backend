5c764f71dfd22eceaac20c5dce19c0eec1ed5462
hossienzr110000 <ghost.zr4848@gmail.com>
2025-09-20 17:07:46 +0330

    changed db

--- call_center_backend/settings.py
+++ call_center_backend/settings.py
@@ -171,17 +171,17 @@ CELERY_BEAT_SCHEDULE = {
 # other codes ...
 
 
-#DATABASES = {
-#  'default': dj_database_url.config(default=os.environ.get("DATABASE_URL")),
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'call_center_db',
-       'USER': 'postgres',
-        'PASSWORD': '@Mirzr4848',
-        'HOST': 'localhost',  # یا IP سرور دیتابیس
-        'PORT': '5432',  # پورت پیش‌فرض PostgreSQL
-        }
-    }
+  'default': dj_database_url.config(default=os.environ.get("DATABASE_URL")),
+}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'call_center_db',
+#       'USER': 'postgres',
+#        'PASSWORD': '@Mirzr4848',
+#        'HOST': 'localhost',  # یا IP سرور دیتابیس
+#        'PORT': '5432',  # پورت پیش‌فرض PostgreSQL
+#        }
+#    }
