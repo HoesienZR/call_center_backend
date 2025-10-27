b3b198633fdf7b45ed2fe49ac055801b30b20298
hossienzr110000 <ghost.zr4848@gmail.com>
2025-10-27 23:02:07 +0330

    Update

--- call_center_backend/settings.py
+++ call_center_backend/settings.py
@@ -205,9 +205,9 @@ CACHES = {
     }
 }
 
- # DATABASES = {
- #   'default': dj_database_url.config(default=os.environ.get("DATABASE_URL")),
- # }
+DATABASES = {
+   'default': dj_database_url.config(default=os.environ.get("DATABASE_URL")),
+}
 
 
 #DATABASES = {
