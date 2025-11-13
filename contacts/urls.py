7087d2f9f4671b7df29b5d2fb227305c2e358ac3
hossienzr110000 <ghost.zr4848@gmail.com>
2025-11-13 17:39:40 +0330

    request new contact api url

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from contacts.views import ContactViewSet
+from contacts.views import ContactViewSet, RequestNewContactView
 
 router = DefaultRouter
 
 router.register(r'contacts', ContactViewSet, basename='contacts')
+
+urlpatterns = [
+    path("contacts/request/", RequestNewContactView.as_view(), name="request-new-contact"),
+]
